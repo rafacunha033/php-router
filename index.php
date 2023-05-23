f08b3f8b1f42719d2f01bc3f8524ccc34b1dec68
rafacunha033 <rafacunha.prof033@gmail.com>
2023-05-23 08:21:04 -0300

    Create new route with Router class

--- index.php
+++ index.php
@@ -1,3 +1,32 @@
 <?php 
 
-echo 'This is the way!';
+include('vendor/autoload.php');
+
+class Router 
+{
+    /** @var array */
+    protected array $routes;
+
+    public function __construct()
+    {}
+
+    public function addRoute($method, $uri)
+    {
+        $this->routes[] = [
+            'method' => $method,
+            'uri'    => $uri
+        ];
+    }
+
+    public function routes()
+    {
+        return $this->routes;
+    }
+}
+
+$router = new Router();
+$router->addRoute("GET", "/");
+
+echo "<pre>";
+    var_dump($router->routes());
+echo "</pre>";
